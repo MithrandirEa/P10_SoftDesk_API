554c9c82852c2ac1c803c9d74a46a5471c4b1c77
MithrandirEa <clscipion@gmail.com>
2026-01-28 17:38:15 +0100

    Création du diagrammede classes

--- src/SoftDesk_API/models.py
+++ src/SoftDesk_API/models.py
@@ -10,3 +10,35 @@ class User(models.Model):
     can_be_contacted = models.BooleanField(default=True)
     can_data_be_shared = models.BooleanField(default=True)
     created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Contributor(models.Model):
+
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    type = models.CharField(max_length=50)
+    contributors = models.ManyToManyField(User, through=Contributor)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Issue(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    priority = models.CharField(max_length=50)
+    tag = models.CharField(max_length=50)
+    status = models.CharField(max_length=50)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+
+class Comment(models.Model):
+    uuid = models.UUIDField(primary_key=True, editable=False)
+    description = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    issue = models.ForeignKey('Issue', on_delete=models.CASCADE)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    project = models.ForeignKey('Project', on_delete=models.CASCADE)
